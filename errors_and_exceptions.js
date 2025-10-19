871f64947dae1797d54e69b37d8d736a157801d5
Ricardo Linhares <155170516+ricardoslinhares@users.noreply.github.com>
2025-10-19 18:37:17 -0300

    Update error message for division by zero

---
+++ errors_and_exceptions.js
@@ -0,0 +1,22 @@
+function div (a,b) {
+    if (b == 0) {
+        throw new RangeError('Can\'t divide by 0.');
+    };
+    return a/b;
+};
+
+let numbers = [10, 40, 0, 20, 50];
+
+for (let i = 0; i < numbers.length; i++) {
+    
+    let result;
+    
+    try {
+        result = div(1000, numbers[i]);
+    } catch (error ) {
+        result = error.message;
+    };
+    
+    console.log(result);
+    
+};
